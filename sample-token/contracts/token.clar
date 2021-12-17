3c1495030c45d4c9d6f414af873bbe0176241c4b
Luiz Alberto <luizalbertocviana@gmail.com>
2021-12-17 07:41:19 -0300

    Luiz/sample token (#5)
    * adds a dummy ERC20-like token smart contract
    
    * changes vscode project setup regarding deno
    
    * adds decrease-allowance function
    
    * wip unit tests for ERC20-like dummy token
    
    * simplifies allowance function return
    
    * adds more unit tests for the token contract

---
+++ sample-token/contracts/token.clar
@@ -0,0 +1,95 @@
+;; token
+;; sample token implementation
+
+(impl-trait .sip-010-v0a.ft-trait)
+
+(define-constant unauthorized-minter u100)
+(define-constant unauthorized-burner u101)
+(define-constant unauthorized-transfer u102)
+(define-constant unauthorized-allowance-query u103)
+(define-constant attempt-to-decrease-inexistent-allowance u104)
+
+(define-constant contract-owner tx-sender)
+
+(define-fungible-token token)
+
+(define-public (mint (amount uint) (to principal))
+  (begin
+    (asserts! (is-eq tx-sender contract-owner) (err unauthorized-minter))
+    (ft-mint? token amount to)))
+
+(define-public (burn (amount uint))
+  (begin
+    (asserts! (is-eq tx-sender contract-owner) (err unauthorized-burner))
+    (ft-burn? token amount tx-sender)))
+
+(define-map approvals {approver: principal, approvee: principal} {amount: uint})
+
+(define-private (approved-transfer? (from principal) (amount uint))
+  (or (is-eq tx-sender from)
+      (match (map-get? approvals {approver: from, approvee: tx-sender})
+        approved-amount-tuple
+        (<= amount (get amount approved-amount-tuple))
+        false)))
+
+(define-private (update-approval (from principal) (transferred-amount uint))
+  (let ((approval-tuple {approver: from, approvee: tx-sender}))
+    (match (map-get? approvals approval-tuple)
+      approved-amount-tuple
+      (map-set approvals approval-tuple {amount: (- (get amount approved-amount-tuple) transferred-amount)})
+      true)))
+
+(define-public (transfer (amount uint) (from principal) (to principal))
+  (begin
+    (asserts! (approved-transfer? from amount) (err unauthorized-transfer))
+    (match (ft-transfer? token amount from to)
+      ok-transfer
+      (if (not (is-eq from tx-sender))
+        (begin
+          (update-approval from amount)
+          (ok true))
+        (ok true))
+      err-transfer
+      (err err-transfer))))
+
+(define-public (approve (approvee principal) (new-approved-amount uint))
+  (let ((approval-tuple {approver: tx-sender, approvee: approvee})
+        (approved-amount-tuple (default-to {amount: u0} (map-get? approvals approval-tuple))))
+    (map-set approvals approval-tuple {amount: (+ (get amount approved-amount-tuple) new-approved-amount)})
+    (ok true)))
+
+(define-public (decrease-allowance (approvee principal) (amount-to-decrease uint))
+  (let ((approval-tuple {approver: tx-sender, approvee: approvee})
+        (approved-amount-tuple-option (map-get? approvals approval-tuple)))
+    (asserts! (is-some approved-amount-tuple-option) (err attempt-to-decrease-inexistent-allowance))
+    (let ((approved-amount-tuple (default-to {amount: u0} approved-amount-tuple-option))
+          (approved-amount (get amount approved-amount-tuple))
+          (new-approved-amount (if (>= amount-to-decrease approved-amount)
+                                   u0
+                                   (- approved-amount amount-to-decrease))))
+      (map-set approvals approval-tuple {amount: new-approved-amount})
+      (ok true))))
+
+(define-read-only (allowance (approver principal) (approvee principal))
+  (begin
+    (asserts! (or (is-eq tx-sender approver)
+                  (is-eq tx-sender approvee)) (err unauthorized-allowance-query))
+    (ok (get amount (default-to {amount: u0} (map-get? approvals {approver: approver, approvee: approvee}))))))
+
+(define-read-only (get-name)
+  (ok "token"))
+
+(define-read-only (get-symbol)
+  (ok "TKN"))
+
+(define-read-only (get-decimals)
+  (ok u2))
+
+(define-read-only (get-balance-of (account principal))
+  (ok (ft-get-balance token account)))
+
+(define-read-only (get-total-supply)
+  (ok (ft-get-supply token)))
+
+(define-read-only (get-token-uri)
+  (ok (some u"www.token.com")))
---
+++ sample-token/tests/token_test.ts
@@ -0,0 +1,184 @@
+import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v0.14.0/index.ts';
+
+Clarinet.test({
+    name: "Ensure the constant read only functions are returning as expected",
+    fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get('deployer')!;
+
+        const uri = chain.callReadOnlyFn('token', 'get-token-uri', [], deployer.address);
+        uri.result.expectOk().expectSome().expectUtf8('www.token.com');
+
+        const decimals = chain.callReadOnlyFn('token', 'get-decimals', [], deployer.address);
+        decimals.result.expectOk().expectUint(2);
+
+        const symbol = chain.callReadOnlyFn('token', 'get-symbol', [], deployer.address);
+        symbol.result.expectOk().expectAscii('TKN');
+
+        const name = chain.callReadOnlyFn('token', 'get-name', [], deployer.address);
+        name.result.expectOk().expectAscii('token');
+    }
+});
+
+Clarinet.test({
+    name: "Ensure mint and burn functions work as expected",
+    fn(chain: Chain, accounts: Map<string, Account>) {
+        const unauthorizedMinter = 100;
+        const unauthorizedBurner = 101;
+
+        const deployer = accounts.get('deployer')!;
+        const wallet1 = accounts.get('wallet_1')!;
+
+        const block1 = chain.mineBlock([
+            Tx.contractCall('token', 'mint', [types.uint(100), types.principal(deployer.address)], deployer.address),
+            Tx.contractCall('token', 'mint', [types.uint(100), types.principal(wallet1.address)], wallet1.address),
+            Tx.contractCall('token', 'mint', [types.uint(50), types.principal(wallet1.address)], deployer.address)
+        ]);
+
+        const [goodMintCall, badMintCall, mintCallToOtherWallet] = block1.receipts;
+
+        goodMintCall.result.expectOk().expectBool(true);
+        badMintCall.result.expectErr().expectUint(unauthorizedMinter);
+        mintCallToOtherWallet.result.expectOk().expectBool(true);
+
+        const supply = chain.callReadOnlyFn('token', 'get-total-supply', [], deployer.address);
+        supply.result.expectOk().expectUint(150);
+
+        let deployerBalance = chain.callReadOnlyFn('token', 'get-balance-of', [types.principal(deployer.address)], deployer.address);
+        deployerBalance.result.expectOk().expectUint(100);
+
+        let wallet1Balance = chain.callReadOnlyFn('token', 'get-balance-of', [types.principal(wallet1.address)], wallet1.address);
+        wallet1Balance.result.expectOk().expectUint(50);
+
+        const block2 = chain.mineBlock([
+            Tx.contractCall('token', 'burn', [types.uint(10)], deployer.address),
+            Tx.contractCall('token', 'burn', [types.uint(10)], wallet1.address),
+            Tx.contractCall('token', 'burn', [types.uint(0)], deployer.address)
+        ]);
+
+        const [goodBurnCall, badBurnCall, zeroBurnCall] = block2.receipts;
+
+        goodBurnCall.result.expectOk().expectBool(true);
+        badBurnCall.result.expectErr().expectUint(unauthorizedBurner);
+        zeroBurnCall.result.expectErr().expectUint(1);
+
+        deployerBalance = chain.callReadOnlyFn('token', 'get-balance-of', [types.principal(deployer.address)], deployer.address);
+        deployerBalance.result.expectOk().expectUint(90);
+
+        wallet1Balance = chain.callReadOnlyFn('token', 'get-balance-of', [types.principal(wallet1.address)], wallet1.address);
+        wallet1Balance.result.expectOk().expectUint(50);
+    }
+});
+
+Clarinet.test({
+    name: "Ensure transfer facilities work as expected",
+    fn(chain: Chain, accounts: Map<string, Account>) {
+        const unauthorizedTransfer = 102;
+        const unauthorizedAllowanceQuery = 103;
+        const attemptToDecreaseInexistentAllowance = 104;
+
+        const deployer = accounts.get('deployer')!;
+
+        const wallet1 = accounts.get('wallet_1')!;
+        const wallet2 = accounts.get('wallet_2')!;
+        const wallet3 = accounts.get('wallet_3')!;
+        const wallet4 = accounts.get('wallet_4')!;
+
+        const block1 = chain.mineBlock([
+            Tx.contractCall('token', 'mint', [types.uint(100), types.principal(wallet1.address)], deployer.address),
+            Tx.contractCall('token', 'mint', [types.uint(100), types.principal(wallet2.address)], deployer.address),
+            Tx.contractCall('token', 'mint', [types.uint(100), types.principal(wallet3.address)], deployer.address)
+        ]);
+
+        const [mintCall1, mintCall2, mintCall3] = block1.receipts;
+
+        mintCall1.result.expectOk().expectBool(true);
+        mintCall2.result.expectOk().expectBool(true);
+        mintCall3.result.expectOk().expectBool(true);
+
+        let wallet1Balance = chain.callReadOnlyFn('token', 'get-balance-of', [types.principal(wallet1.address)], wallet1.address);
+        wallet1Balance.result.expectOk().expectUint(100);
+
+        let wallet2Balance = chain.callReadOnlyFn('token', 'get-balance-of', [types.principal(wallet2.address)], wallet2.address);
+        wallet2Balance.result.expectOk().expectUint(100);
+
+        let wallet3Balance = chain.callReadOnlyFn('token', 'get-balance-of', [types.principal(wallet3.address)], wallet3.address);
+        wallet3Balance.result.expectOk().expectUint(100);
+
+        const block2 = chain.mineBlock([
+            Tx.contractCall('token', 'transfer', [types.uint(10), types.principal(wallet1.address), types.principal(wallet3.address)], wallet1.address),
+            Tx.contractCall('token', 'transfer', [types.uint(10), types.principal(wallet2.address), types.principal(wallet3.address)], wallet2.address),
+            Tx.contractCall('token', 'transfer', [types.uint(10), types.principal(wallet3.address), types.principal(wallet1.address)], wallet1.address)
+        ]);
+
+        const [goodTransferCall1, goodTransferCall2, badTransferCall1] = block2.receipts;
+
+        goodTransferCall1.result.expectOk().expectBool(true);
+        goodTransferCall2.result.expectOk().expectBool(true);
+        badTransferCall1.result.expectErr().expectUint(unauthorizedTransfer);
+
+        wallet1Balance = chain.callReadOnlyFn('token', 'get-balance-of', [types.principal(wallet1.address)], wallet1.address);
+        wallet1Balance.result.expectOk().expectUint(90);
+
+        wallet2Balance = chain.callReadOnlyFn('token', 'get-balance-of', [types.principal(wallet2.address)], wallet2.address);
+        wallet2Balance.result.expectOk().expectUint(90);
+
+        wallet3Balance = chain.callReadOnlyFn('token', 'get-balance-of', [types.principal(wallet3.address)], wallet3.address);
+        wallet3Balance.result.expectOk().expectUint(120);
+
+        const block3 = chain.mineBlock([
+            Tx.contractCall('token', 'approve', [types.principal(wallet1.address), types.uint(20)], wallet3.address),
+            Tx.contractCall('token', 'approve', [types.principal(wallet2.address), types.uint(20)], wallet3.address)
+        ]);
+
+        const [approveCall1, approveCall2] = block3.receipts;
+
+        approveCall1.result.expectOk().expectBool(true);
+        approveCall2.result.expectOk().expectBool(true);
+
+        const allowanceQuery1 = chain.callReadOnlyFn('token', 'allowance', [types.principal(wallet3.address), types.principal(wallet1.address)], wallet3.address);
+        allowanceQuery1.result.expectOk().expectUint(20);
+
+        const allowanceQuery2 = chain.callReadOnlyFn('token', 'allowance', [types.principal(wallet3.address), types.principal(wallet2.address)], wallet2.address);
+        allowanceQuery2.result.expectOk().expectUint(20);
+
+        const badAllowanceQuery = chain.callReadOnlyFn('token', 'allowance', [types.principal(wallet3.address), types.principal(wallet2.address)], wallet1.address);
+        badAllowanceQuery.result.expectErr().expectUint(unauthorizedAllowanceQuery);
+
+        const block4 = chain.mineBlock([
+            Tx.contractCall('token', 'transfer', [types.uint(5), types.principal(wallet3.address), types.principal(wallet4.address)], wallet1.address),
+            Tx.contractCall('token', 'transfer', [types.uint(5), types.principal(wallet3.address), types.principal(wallet4.address)], wallet2.address),
+            Tx.contractCall('token', 'transfer', [types.uint(5), types.principal(wallet3.address), types.principal(wallet4.address)], wallet4.address)
+        ]);
+
+        const [goodTransferCall3, goodTransferCall4, badTransferCall2] = block4.receipts;
+
+        goodTransferCall3.result.expectOk().expectBool(true);
+        goodTransferCall4.result.expectOk().expectBool(true);
+        badTransferCall2.result.expectErr().expectUint(unauthorizedTransfer);
+
+        wallet3Balance = chain.callReadOnlyFn('token', 'get-balance-of', [types.principal(wallet3.address)], wallet3.address);
+        wallet3Balance.result.expectOk().expectUint(110);
+
+        const wallet4Balance = chain.callReadOnlyFn('token', 'get-balance-of', [types.principal(wallet4.address)], wallet4.address);
+        wallet4Balance.result.expectOk().expectUint(10);
+
+        const allowanceQuery3 = chain.callReadOnlyFn('token', 'allowance', [types.principal(wallet3.address), types.principal(wallet1.address)], wallet1.address);
+        allowanceQuery3.result.expectOk().expectUint(15);
+
+        const allowanceQuery4 = chain.callReadOnlyFn('token', 'allowance', [types.principal(wallet3.address), types.principal(wallet2.address)], wallet3.address);
+        allowanceQuery4.result.expectOk().expectUint(15);
+
+        const block5 = chain.mineBlock([
+            Tx.contractCall('token', 'decrease-allowance', [types.principal(wallet1.address), types.uint(10)], wallet3.address),
+            Tx.contractCall('token', 'decrease-allowance', [types.principal(wallet1.address), types.uint(10)], wallet4.address)
+        ]);
+
+        const [goodDecreaseAllowanceCall, badDecreaseAllowanceCall] = block5.receipts;
+
+        goodDecreaseAllowanceCall.result.expectOk().expectBool(true);
+        badDecreaseAllowanceCall.result.expectErr().expectUint(attemptToDecreaseInexistentAllowance);
+
+        const allowanceQuery5 = chain.callReadOnlyFn('token', 'allowance', [types.principal(wallet3.address), types.principal(wallet1.address)], wallet1.address);
+        allowanceQuery5.result.expectOk().expectUint(5);
+    }
+});
