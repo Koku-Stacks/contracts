61af4ba4a2cd1dbd623d74d035f88274d6fa87d4
Luiz Alberto do Carmo Viana <luizalbertocviana@gmail.com>
2022-01-11 09:43:33 -0300

    adds import to deno's assertEquals

--- koku/tests/token_test.ts
+++ koku/tests/token_test.ts
@@ -1,4 +1,5 @@
 import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v0.14.0/index.ts';
+import { assertEquals } from "https://deno.land/std@0.120.0/testing/asserts.ts";
 
 Clarinet.test({
     name: "Ensure the token uri facilities work as expected",
