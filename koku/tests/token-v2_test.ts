0dbf9d405c99fc60aab8de18b86c3d18d18cc3ee
Luiz Alberto do Carmo Viana <luizalbertocviana@gmail.com>
2022-01-25 11:00:05 -0300

    wip first version of token-v2_test.ts

--- koku/tests/token-v2_test.ts
+++ koku/tests/token-v2_test.ts
@@ -1,6 +1,76 @@
 import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v0.14.0/index.ts';
 import { assertEquals } from "https://deno.land/std@0.120.0/testing/asserts.ts";
 
+const onlyOwnerCanAddAuthorizedContracts = 100;
+const onlyOwnerCanRevokeAuthorizedContracts = 101;
+const unauthorizedContract = 102;
+const contractAlreadyAuthorized = 103;
+const contractIsNotAuthorized = 104;
+const onlyAuthorizedContractsCanSetUri = 105;
+const onlyAuthorizedContractsCanMintToken = 106;
+const unauthorizedTransfer = 107;
+const ownershipTransferNotSubmittedByOwner = 108;
+const anotherOwnershipTransferIsSubmitted = 109;
+const ownershipTransferNotCancelledByOwner = 110;
+const noOwnershipTransferToCancel = 111;
+const noOwnershipTransferToConfirm = 112;
+const ownershipTransferNotConfirmedByNewOwner = 113;
+
+Clarinet.test({
+    name: "Ensure mint and burn functions work as expected",
+    fn(chain: Chain, accounts: Map<string, Account>) {
+        const unauthorizedMinter = 100;
+
+        const deployer = accounts.get('deployer')!;
+        const wallet1 = accounts.get('wallet_1')!;
+
+        const block1 = chain.mineBlock([
+            Tx.contractCall('token-v2', 'mint', [types.uint(100), types.principal(deployer.address)], deployer.address)
+        ]);
+
+        const [badMintCall1] = block1.receipts;
+
+        badMintCall1.result.expectErr().expectUint(onlyAuthorizedContractsCanMintToken);
+
+        const block2 = chain.mineBlock([
+            Tx.contractCall('minting', 'mint', [types.uint(100), types.principal(deployer.address)], deployer.address),
+            Tx.contractCall('minting', 'mint', [types.uint(100), types.principal(wallet1.address)], wallet1.address),
+            Tx.contractCall('minting', 'mint', [types.uint(50), types.principal(wallet1.address)], deployer.address)
+        ]);
+
+        const [goodMintCall, badMintCall2, mintCallToOtherWallet] = block2.receipts;
+
+        goodMintCall.result.expectOk().expectBool(true);
+        badMintCall2.result.expectErr().expectUint(unauthorizedMinter);
+        mintCallToOtherWallet.result.expectOk().expectBool(true);
+
+        const supply = chain.callReadOnlyFn('token-v2', 'get-total-supply', [], deployer.address);
+        supply.result.expectOk().expectUint(150);
+
+        let deployerBalance = chain.callReadOnlyFn('token-v2', 'get-balance', [types.principal(deployer.address)], deployer.address);
+        deployerBalance.result.expectOk().expectUint(100);
+
+        let wallet1Balance = chain.callReadOnlyFn('token-v2', 'get-balance', [types.principal(wallet1.address)], wallet1.address);
+        wallet1Balance.result.expectOk().expectUint(50);
+
+        const block3 = chain.mineBlock([
+            Tx.contractCall('token-v2', 'burn', [types.uint(10)], deployer.address),
+            Tx.contractCall('token-v2', 'burn', [types.uint(0)], deployer.address)
+        ]);
+
+        const [goodBurnCall, zeroBurnCall] = block3.receipts;
+
+        goodBurnCall.result.expectOk().expectBool(true);
+        zeroBurnCall.result.expectErr().expectUint(1);
+
+        deployerBalance = chain.callReadOnlyFn('token-v2', 'get-balance', [types.principal(deployer.address)], deployer.address);
+        deployerBalance.result.expectOk().expectUint(90);
+
+        wallet1Balance = chain.callReadOnlyFn('token-v2', 'get-balance', [types.principal(wallet1.address)], wallet1.address);
+        wallet1Balance.result.expectOk().expectUint(50);
+    }
+});
+
 Clarinet.test({
     name: "Ensure the constant read only functions are returning as expected",
     fn(chain: Chain, accounts: Map<string, Account>) {
