0b5737c0fc552e79c0295e44ca0fb455772c6c25
Luiz Alberto do Carmo Viana <luizalbertocviana@gmail.com>
2022-01-27 09:00:44 -0300

    adds ownership logic and the corresponding unit tests for minting contract

--- koku/contracts/minting.clar
+++ koku/contracts/minting.clar
@@ -1,9 +1,45 @@
 (define-constant unauthorized-minter u100)
 (define-constant insuficcient-tokens-to-mint u101)
+(define-constant ownership-transfer-not-submitted-by-owner u107)
+(define-constant another-ownership-transfer-is-submitted u108)
+(define-constant ownership-transfer-not-cancelled-by-owner u109)
+(define-constant no-ownership-transfer-to-cancel u110)
+(define-constant no-ownership-transfer-to-confirm u111)
+(define-constant ownership-transfer-not-confirmed-by-new-owner u112)
 
 (define-constant this-contract (as-contract tx-sender))
 
 (contract-call? .token-v2 add-authorized-contract this-contract)
+
+(define-data-var owner principal tx-sender)
+(define-data-var submitted-new-owner (optional principal) none)
+
+(define-read-only (get-owner)
+  (var-get owner))
+
+(define-public (submit-ownership-transfer (new-owner principal))
+  (begin
+    (asserts! (is-eq (get-owner) tx-sender) (err ownership-transfer-not-submitted-by-owner))
+    (asserts! (is-none (var-get submitted-new-owner)) (err another-ownership-transfer-is-submitted))
+    (var-set submitted-new-owner (some new-owner))
+    (ok true)))
+
+(define-public (cancel-ownership-transfer)
+  (begin
+    (asserts! (is-eq (get-owner) tx-sender) (err ownership-transfer-not-cancelled-by-owner))
+    (asserts! (is-some (var-get submitted-new-owner)) (err no-ownership-transfer-to-cancel))
+    (var-set submitted-new-owner none)
+    (ok true)))
+
+(define-public (confirm-ownership-transfer)
+  (match (var-get submitted-new-owner)
+    new-owner
+    (begin
+      (asserts! (is-eq (some tx-sender) (var-get submitted-new-owner)) (err ownership-transfer-not-confirmed-by-new-owner))
+      (var-set submitted-new-owner none)
+      (var-set owner new-owner)
+      (ok true))
+    (err no-ownership-transfer-to-confirm)))
 
 (define-data-var remaining-tokens-to-mint uint u21000000000000)
 
@@ -12,9 +48,6 @@
 
 (define-private (decrease-remaining-tokens-to-mint (amount uint))
   (var-set remaining-tokens-to-mint (- (get-remaining-tokens-to-mint) amount)))
-
-(define-read-only (get-owner)
-  (contract-call? .token-v2 get-owner))
 
 (define-public (mint (amount uint) (to principal))
   (begin
--- koku/tests/minting_test.ts
+++ koku/tests/minting_test.ts
@@ -1,10 +1,141 @@
 import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v0.14.0/index.ts';
 
+const insufficientTokensToMint = 101;
+const ownershipTransferNotSubmittedByOwner = 107;
+const anotherOwnershipTransferIsSubmitted = 108;
+const ownershipTransferNotCancelledByOwner = 109;
+const noOwnershipTransferToCancel = 110;
+const noOwnershipTransferToConfirm = 111;
+const ownershipTransferNotConfirmedByNewOwner = 112;
+
+Clarinet.test({
+    name: "Ensure the ownership facilities work as expected",
+    fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get('deployer')!;
+        const wallet1 = accounts.get('wallet_1')!;
+        const wallet2 = accounts.get('wallet_2')!;
+
+        let tokenOwner = chain.callReadOnlyFn('minting', 'get-owner', [], wallet2.address);
+        tokenOwner.result.expectPrincipal(deployer.address);
+
+        const block1 = chain.mineBlock([
+            Tx.contractCall('minting',
+                            'submit-ownership-transfer',
+                            [types.principal(wallet1.address)],
+                            wallet1.address)
+        ]);
+
+        const [badSubmitOwnershipTransferCall1] = block1.receipts;
+
+        badSubmitOwnershipTransferCall1.result.expectErr().expectUint(ownershipTransferNotSubmittedByOwner);
+
+        const block2 = chain.mineBlock([
+            Tx.contractCall('minting',
+                            'submit-ownership-transfer',
+                            [types.principal(wallet1.address)],
+                            deployer.address)
+        ]);
+
+        const [goodSubmitOwnershipTransferCall1] = block2.receipts;
+
+        goodSubmitOwnershipTransferCall1.result.expectOk().expectBool(true);
+
+        const block3 = chain.mineBlock([
+            Tx.contractCall('minting',
+                            'submit-ownership-transfer',
+                            [types.principal(wallet2.address)],
+                            deployer.address)
+        ]);
+
+        const [badSubmitOwnershipTransferCall3] = block3.receipts;
+
+        badSubmitOwnershipTransferCall3.result.expectErr().expectUint(anotherOwnershipTransferIsSubmitted);
+
+        const block4 = chain.mineBlock([
+            Tx.contractCall('minting',
+                            'cancel-ownership-transfer',
+                            [],
+                            wallet2.address)
+        ]);
+
+        const [badCancelOwnershipTransferCall1] = block4.receipts;
+
+        badCancelOwnershipTransferCall1.result.expectErr().expectUint(ownershipTransferNotCancelledByOwner);
+
+        const block5 = chain.mineBlock([
+            Tx.contractCall('minting',
+                            'cancel-ownership-transfer',
+                            [],
+                            deployer.address)
+        ]);
+
+        const [goodCancelOwnershipTransferCall] = block5.receipts;
+
+        goodCancelOwnershipTransferCall.result.expectOk().expectBool(true);
+
+        const block6 = chain.mineBlock([
+            Tx.contractCall('minting',
+                            'confirm-ownership-transfer',
+                            [],
+                            wallet1.address)
+        ]);
+
+        const [badConfirmOwnershipTransferCall1] = block6.receipts;
+
+        badConfirmOwnershipTransferCall1.result.expectErr().expectUint(noOwnershipTransferToConfirm);
+
+        const block7 = chain.mineBlock([
+            Tx.contractCall('minting',
+                            'cancel-ownership-transfer',
+                            [],
+                            deployer.address)
+        ]);
+
+        const [badCancelOwnershipTransferCall2] = block7.receipts;
+
+        badCancelOwnershipTransferCall2.result.expectErr().expectUint(noOwnershipTransferToCancel);
+
+        const block8 = chain.mineBlock([
+            Tx.contractCall('minting',
+                            'submit-ownership-transfer',
+                            [types.principal(wallet2.address)],
+                            deployer.address)
+        ]);
+
+        const [goodSubmitOwnershipTransferCall2] = block8.receipts;
+
+        goodSubmitOwnershipTransferCall2.result.expectOk().expectBool(true);
+
+        const block9 = chain.mineBlock([
+            Tx.contractCall('minting',
+                            'confirm-ownership-transfer',
+                            [],
+                            deployer.address)
+        ]);
+
+        const [badConfirmOwnershipTransferCall2] = block9.receipts;
+
+        badConfirmOwnershipTransferCall2.result.expectErr().expectUint(ownershipTransferNotConfirmedByNewOwner);
+
+        const block10 = chain.mineBlock([
+            Tx.contractCall('minting',
+                            'confirm-ownership-transfer',
+                            [],
+                            wallet2.address)
+        ]);
+
+        const [goodConfirmOwnershipTransferCall] = block10.receipts;
+
+        goodConfirmOwnershipTransferCall.result.expectOk().expectBool(true);
+
+        tokenOwner = chain.callReadOnlyFn('minting', 'get-owner', [], wallet2.address);
+        tokenOwner.result.expectPrincipal(wallet2.address);
+    }
+});
+
 Clarinet.test({
     name: "Ensure the token max supply constraint is respected",
     fn(chain: Chain, accounts: Map<string, Account>) {
-        const insufficientTokensToMint = 101;
-
         const deployer = accounts.get('deployer')!;
 
         const maxTokensToMint = 21_000_000_000_000;
@@ -87,8 +218,6 @@ Clarinet.test({
 Clarinet.test({
     name: "Ensure that token max supply is respected when there are multiple mint calls in the same block",
     fn(chain: Chain, accounts: Map<string, Account>) {
-        const insufficientTokensToMint = 101;
-
         const deployer = accounts.get('deployer')!;
         const wallet1 = accounts.get('wallet_1')!;
         const wallet2 = accounts.get('wallet_2')!;
