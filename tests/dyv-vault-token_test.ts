b7cac067feb422fbaea897ef0360ccd40d1177eb
Saad Tahir <saadtahir28@gmail.com>
2022-02-21 13:49:29 +0500

    Update dyv-vault-token_test.ts

--- tests/dyv-vault-token_test.ts
+++ tests/dyv-vault-token_test.ts
@@ -3,24 +3,101 @@ import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarine
 import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
 
 Clarinet.test({
-    name: "Ensure that <...>",
+    name: "Ensure that set-owner can only be called by contract owner",
     async fn(chain: Chain, accounts: Map<string, Account>) {
-        let block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
+        const deployer = accounts.get('deployer')!;
+        const userA = accounts.get('wallet_1')!;
+        
+        let call = chain.mineBlock([
+            Tx.contractCall(
+                'dyv-vault-token',
+                'set-owner',
+                [ types.principal(userA.address)],
+                userA.address)
         ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 2);
 
-        block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
+        call.receipts[0].result.expectErr().expectUint(1000)
+    },
+});
+
+Clarinet.test({
+    name: "Ensure that set-token-uri can only be called by contract owner",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get('deployer')!;
+        const userA = accounts.get('wallet_1')!;
+        
+        let call = chain.mineBlock([
+            Tx.contractCall(
+                'dyv-vault-token',
+                'set-token-uri',
+                [ types.utf8("www.dyv.com")],
+                userA.address)
         ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 3);
+
+        call.receipts[0].result.expectErr().expectUint(1000)
+    },
+});
+
+Clarinet.test({
+    name: "Ensure that transfer can only be called by token owner",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get('deployer')!;
+        const userA = accounts.get('wallet_1')!;
+        
+        let call = chain.mineBlock([
+            Tx.contractCall(
+                'dyv-vault-token',
+                'transfer',
+                [ 
+                    types.uint(100),
+                    types.principal(userA.address),
+                    types.principal(deployer.address),
+                    types.none()
+                ],
+                deployer.address)
+        ]);
+
+        call.receipts[0].result.expectErr().expectUint(1001)
+    },
+});
+
+Clarinet.test({
+    name: "Ensure that mint can only be called by approved contracts",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get('deployer')!;
+        const userA = accounts.get('wallet_1')!;
+        
+        let call = chain.mineBlock([
+            Tx.contractCall(
+                'dyv-vault-token',
+                'mint',
+                [ 
+                    types.uint(100),
+                    types.principal(userA.address),
+                ],
+                userA.address)
+        ]);
+
+        call.receipts[0].result.expectErr().expectUint(1000)
+    },
+});
+
+Clarinet.test({
+    name: "Ensure that burn can only be called by approved contracts",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get('deployer')!;
+        const userA = accounts.get('wallet_1')!;
+        
+        let call = chain.mineBlock([
+            Tx.contractCall(
+                'dyv-vault-token',
+                'burn',
+                [ 
+                    types.uint(100),
+                ],
+                userA.address)
+        ]);
+
+        call.receipts[0].result.expectErr().expectUint(1000)
     },
 });
