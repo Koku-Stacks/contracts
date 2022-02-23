f2cbf19acce663ee0540c495ec01742f17904d49
Saad Tahir <saadtahir28@gmail.com>
2022-02-23 14:42:18 +0500

    Update vault_test.ts

--- tests/vault_test.ts
+++ tests/vault_test.ts
@@ -2,8 +2,6 @@
 import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v0.14.0/index.ts';
 import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
 
-const token = "ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM.token"
-
 Clarinet.test({
     name: "Ensure that set-owner can only be called by contract owner",
     async fn(chain: Chain, accounts: Map<string, Account>) {
@@ -27,6 +25,7 @@ Clarinet.test({
     async fn(chain: Chain, accounts: Map<string, Account>) {
         const deployer = accounts.get('deployer')!;
         const userA = accounts.get('wallet_1')!;
+        const token = `${deployer.address}.token`;
         
         let call = chain.mineBlock([
             Tx.contractCall(
