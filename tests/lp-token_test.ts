dee3c0f4c52317ceaed17533f01f3016a0c69600
Saad Tahir <86647610+SaadTahir7@users.noreply.github.com>
2022-02-24 12:29:01 +0500

    Added test cases for owner trait

--- tests/lp-token_test.ts
+++ tests/lp-token_test.ts
@@ -21,6 +21,122 @@ Clarinet.test({
 });
 
 Clarinet.test({
+    name: "Ensure that submit ownership can only be called by owner",
+    fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get('deployer')!;
+        const userA = accounts.get('wallet_1')!;
+        const userB = accounts.get('wallet_2')!;
+        
+        let call = chain.mineBlock(
+            [
+                Tx.contractCall('lp-token',
+                'submit-ownership-transfer',
+                [types.principal(userB.address)],
+                userB.address)
+            ]);
+        call.receipts[0].result.expectErr().expectUint(1000);
+    }
+})
+
+Clarinet.test({
+    name: "Ensure that submit ownership transfer cannot be called again if already submitted",
+    fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get('deployer')!;
+        const userA = accounts.get('wallet_1')!;
+        const userB = accounts.get('wallet_2')!;
+        
+        let call = chain.mineBlock(
+            [
+                Tx.contractCall('lp-token',
+                'submit-ownership-transfer',
+                [types.principal(userB.address)],
+                deployer.address),
+                Tx.contractCall('lp-token',
+                'submit-ownership-transfer',
+                [types.principal(userB.address)],
+                deployer.address)
+            ]);
+        call.receipts[1].result.expectErr().expectUint(2001);
+    }
+})
+
+Clarinet.test({
+    name: "Ensure that cancel ownership can only be called by owner",
+    fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get('deployer')!;
+        const userA = accounts.get('wallet_1')!;
+        const userB = accounts.get('wallet_2')!;
+        
+        let call = chain.mineBlock(
+            [
+                Tx.contractCall('lp-token',
+                'cancel-ownership-transfer',
+                [],
+                userB.address)
+            ]);
+        call.receipts[0].result.expectErr().expectUint(1000);
+    }
+})
+
+Clarinet.test({
+    name: "Ensure that cancel ownership transfer cannot be called for non submitted user",
+    fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get('deployer')!;
+        const userA = accounts.get('wallet_1')!;
+        const userB = accounts.get('wallet_2')!;
+        
+        let call = chain.mineBlock(
+            [
+                Tx.contractCall('lp-token',
+                'cancel-ownership-transfer',
+                [],
+                deployer.address)
+            ]);
+        call.receipts[0].result.expectErr().expectUint(2002);
+    }
+})
+
+Clarinet.test({
+    name: "Ensure that confirm ownership can only be called when a transfer has been submitted",
+    fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get('deployer')!;
+        const userA = accounts.get('wallet_1')!;
+        const userB = accounts.get('wallet_2')!;
+        
+        let call = chain.mineBlock(
+            [
+                Tx.contractCall('lp-token',
+                'confirm-ownership-transfer',
+                [],
+                userA.address)
+            ]);
+        call.receipts[0].result.expectErr().expectUint(2003);
+    }
+})
+
+Clarinet.test({
+    name: "Ensure that confirm ownership transfer can only be called by submitted owner",
+    fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get('deployer')!;
+        const userA = accounts.get('wallet_1')!;
+        const userB = accounts.get('wallet_2')!;
+        
+        let call = chain.mineBlock(
+            [
+                Tx.contractCall('lp-token',
+                'submit-ownership-transfer',
+                [types.principal(userB.address)],
+                deployer.address),
+                Tx.contractCall('lp-token',
+                'confirm-ownership-transfer',
+                [],
+                userA.address)
+            ]);
+        call.receipts[1].result.expectErr().expectUint(2000);
+    }
+})
+
+Clarinet.test({
     name: "Ensure that transfer can only be called by token owner",
     async fn(chain: Chain, accounts: Map<string, Account>) {
         const deployer = accounts.get('deployer')!;
--- tests/vault_test.ts
+++ tests/vault_test.ts
@@ -3,6 +3,122 @@ import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarine
 import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
 
 Clarinet.test({
+    name: "Ensure that submit ownership can only be called by owner",
+    fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get('deployer')!;
+        const userA = accounts.get('wallet_1')!;
+        const userB = accounts.get('wallet_2')!;
+        
+        let call = chain.mineBlock(
+            [
+                Tx.contractCall('vault',
+                'submit-ownership-transfer',
+                [types.principal(userB.address)],
+                userB.address)
+            ]);
+        call.receipts[0].result.expectErr().expectUint(1000);
+    }
+})
+
+Clarinet.test({
+    name: "Ensure that submit ownership transfer cannot be called again if already submitted",
+    fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get('deployer')!;
+        const userA = accounts.get('wallet_1')!;
+        const userB = accounts.get('wallet_2')!;
+        
+        let call = chain.mineBlock(
+            [
+                Tx.contractCall('vault',
+                'submit-ownership-transfer',
+                [types.principal(userB.address)],
+                deployer.address),
+                Tx.contractCall('vault',
+                'submit-ownership-transfer',
+                [types.principal(userB.address)],
+                deployer.address)
+            ]);
+        call.receipts[1].result.expectErr().expectUint(2001);
+    }
+})
+
+Clarinet.test({
+    name: "Ensure that cancel ownership can only be called by owner",
+    fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get('deployer')!;
+        const userA = accounts.get('wallet_1')!;
+        const userB = accounts.get('wallet_2')!;
+        
+        let call = chain.mineBlock(
+            [
+                Tx.contractCall('vault',
+                'cancel-ownership-transfer',
+                [],
+                userB.address)
+            ]);
+        call.receipts[0].result.expectErr().expectUint(1000);
+    }
+})
+
+Clarinet.test({
+    name: "Ensure that cancel ownership transfer cannot be called for non submitted user",
+    fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get('deployer')!;
+        const userA = accounts.get('wallet_1')!;
+        const userB = accounts.get('wallet_2')!;
+        
+        let call = chain.mineBlock(
+            [
+                Tx.contractCall('vault',
+                'cancel-ownership-transfer',
+                [],
+                deployer.address)
+            ]);
+        call.receipts[0].result.expectErr().expectUint(2002);
+    }
+})
+
+Clarinet.test({
+    name: "Ensure that confirm ownership can only be called when a transfer has been submitted",
+    fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get('deployer')!;
+        const userA = accounts.get('wallet_1')!;
+        const userB = accounts.get('wallet_2')!;
+        
+        let call = chain.mineBlock(
+            [
+                Tx.contractCall('vault',
+                'confirm-ownership-transfer',
+                [],
+                userA.address)
+            ]);
+        call.receipts[0].result.expectErr().expectUint(2003);
+    }
+})
+
+Clarinet.test({
+    name: "Ensure that confirm ownership transfer can only be called by submitted owner",
+    fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get('deployer')!;
+        const userA = accounts.get('wallet_1')!;
+        const userB = accounts.get('wallet_2')!;
+        
+        let call = chain.mineBlock(
+            [
+                Tx.contractCall('vault',
+                'submit-ownership-transfer',
+                [types.principal(userB.address)],
+                deployer.address),
+                Tx.contractCall('vault',
+                'confirm-ownership-transfer',
+                [],
+                userA.address)
+            ]);
+        call.receipts[1].result.expectErr().expectUint(2000);
+    }
+})
+
+Clarinet.test({
     name: "Ensure that deposit and withdraw are working fine",
     async fn(chain: Chain, accounts: Map<string, Account>) {
         const deployer = accounts.get('deployer')!;
