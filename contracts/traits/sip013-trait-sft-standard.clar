275434d80142e325f86fa77839ee0fe8ccf3ab77
Ali Raza <aliraza.ali954@gmail.com>
2022-03-06 17:25:38 +0500

    Updated token-option sc and also its unit testing
    Updated the token-option smart contract according to the advised changes from Luiz and Saad
    Also performed the unit testing for mint, burn, transfer and set-token-uri functions, all the unit testing are working fine and giving out the desired result output.

--- contracts/traits/sip013-trait-sft-standard.clar
+++ contracts/traits/sip013-trait-sft-standard.clar
@@ -2,7 +2,7 @@
 ;; sft-trait
 ;; <add a description here>
 
-(define-trait sft-trait
+(define-trait sip013-trait-sft-standard
 	(
 		;; Get a token type balance of the passed principal.
 		(get-balance (uint principal) (response uint uint))
@@ -25,11 +25,14 @@
 		;; Transfer from one principal to another.
 		(transfer (uint uint principal principal) (response bool uint))
 
-        ;; Mint the token and verify the address response
-        (mint (uint uint principal) (response bool uint))
+		;; Transfer from one principal to another with a memo.
+		(transfer-memo (uint uint principal principal (buff 34)) (response bool uint))
 
-        ;; Burn the token when not needed and verify the address response
-        (burn (uint uint principal) (response bool uint))  
+		;; Transfer many tokens at once.
+		(transfer-many ((list 200 {token-id: uint, amount: uint, sender: principal, recipient: principal})) (response bool uint))
+
+		;; Transfer many tokens at once with memos.
+		(transfer-many-memo ((list 200 {token-id: uint, amount: uint, sender: principal, recipient: principal, memo: (buff 34)})) (response bool uint))
 	)
 )
 
--- tests/token-option_test.ts
+++ tests/token-option_test.ts
@@ -2,25 +2,269 @@
 import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v0.14.0/index.ts';
 import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
 
-Clarinet.test({
-    name: "Ensure that <...>",
-    async fn(chain: Chain, accounts: Map<string, Account>) {
-        let block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 2);
+const ERR_NOT_AUTHORIZED = 1000; 
+const ERR_INVALID_SENDER = 111;
+const ERR_INSUFFICIENT_BALANCE = 110;
+const ERR_CONTRACT_OWNER_ONLY = 103;
+const ERR_INSUFFICIENT_TOKENS_TO_MINT = 108;
 
-        block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
+const defaultPaymentAssetContract = 'token-option';
+
+function mintFt({ chain, deployer, amount, token_id, supply, recipient, paymentAssetContract = defaultPaymentAssetContract }: { chain: Chain, deployer: Account, amount: number, token_id: number, supply: number, recipient: Account, paymentAssetContract?: string }) {
+    const block = chain.mineBlock([
+        Tx.contractCall(paymentAssetContract, 'mint', [types.uint(token_id), types.uint(amount), types.principal(recipient.address), types.uint(supply)], deployer.address),
+    ]);
+    block.receipts[0].result.expectOk();
+    const ftMintEvent = block.receipts[0].events[0].ft_mint_event;
+    const [paymentAssetContractPrincipal, paymentAssetId] = ftMintEvent.asset_identifier.split('::');
+    return { paymentAssetContract: paymentAssetContractPrincipal, paymentAssetId, block };
+}
+
+
+
+Clarinet.test({
+    name: "Ensure that set-token-uri can only be called by owner",
+    fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get('deployer')!;
+        const userA = accounts.get('wallet_1')!;
+        const userB = accounts.get('wallet_2')!;
+        
+        let call = chain.mineBlock(
+            [
+                Tx.contractCall('token-option',
+                'set-token-uri',
+                [types.utf8("https://dy.finance/")],
+                userA.address),
+            ]);
+        call.receipts[0].result.expectErr().expectUint(ERR_NOT_AUTHORIZED);
+    }
+})
+
+Clarinet.test({
+    name: "Ensure that transfer can only be called by token owner",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get('deployer')!;
+        const userA = accounts.get('wallet_1')!;
+        
+        let call = chain.mineBlock([
+            Tx.contractCall(
+                'token-option',
+                'transfer',
+                [ 
+                    types.uint(101),
+                    types.uint(1000),
+                    types.principal(userA.address),
+                    types.principal(deployer.address),
+                    
+                ],
+                deployer.address)
         ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 3);
+
+        call.receipts[0].result.expectErr().expectUint(ERR_INVALID_SENDER);
+    },
+});
+
+
+Clarinet.test({
+    name: "Ensure that transfer is working!",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get('deployer')!;
+        const userA = accounts.get('wallet_1')!;
+        const Price = 50;
+        const { paymentAssetContract, paymentAssetId } = mintFt({ chain, deployer, amount: 100, token_id: 101, supply: 20, recipient: deployer});
+        // chain: Chain, deployer: Account, amount: number, token_id: number, supply: number, recipient: Account, paymentAssetContract?: string
+        // assertEquals(paymentAssetContract, "abc");
+        
+        let call = chain.mineBlock([
+            // Tx.contractCall(
+            //     'token-option',
+            //     'mint',
+            //     [ 
+            //         types.uint(101),
+            //         types.uint(100),
+            //         types.principal(deployer.address),
+            //         types.uint(20),
+            //     ],
+            //     deployer.address),
+            Tx.contractCall(
+                'token-option',
+                'transfer',
+                [ 
+                    types.uint(101),
+                    types.uint(Price),
+                    types.principal(deployer.address),
+                    types.principal(userA.address),
+                    
+                ],
+                deployer.address)
+        ]);
+
+        // call.receipts[0].result.expectOk();
+        call.receipts[0].result.expectOk().expectBool(true);
+
+        call.receipts[0].events.expectFungibleTokenTransferEvent(Price, deployer.address, userA.address, paymentAssetId);
+    },
+});
+
+Clarinet.test({
+    name: "Ensure that transfer is not more than the balance!",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get('deployer')!;
+        const userA = accounts.get('wallet_1')!;
+        
+        let call = chain.mineBlock([
+            Tx.contractCall(
+                'token-option',
+                'mint',
+                [ 
+                    types.uint(101),
+                    types.uint(100),
+                    types.principal(deployer.address),
+                    types.uint(20),
+                ],
+                deployer.address),
+            Tx.contractCall(
+                'token-option',
+                'transfer',
+                [ 
+                    types.uint(101),
+                    types.uint(1000),
+                    types.principal(deployer.address),
+                    types.principal(userA.address),
+                    
+                ],
+                deployer.address)
+        ]);
+
+        call.receipts[0].result.expectOk();
+        call.receipts[1].result.expectErr().expectUint(ERR_INSUFFICIENT_BALANCE);
+    },
+});
+
+Clarinet.test({
+    name: "Ensure that minting function is working",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get('deployer')!;
+        const userA = accounts.get('wallet_1')!;
+        
+        let call = chain.mineBlock([
+            Tx.contractCall(
+                'token-option',
+                'mint',
+                [ 
+                    types.uint(1001),
+                    types.uint(10),
+                    types.principal(userA.address),
+                    types.uint(20),
+                ],
+                deployer.address),
+        ]);
+
+        call.receipts[0].result.expectOk();
+        call.receipts[0].events.expectFungibleTokenMintEvent(10, userA.address, "sft");
+
+
+    },
+});
+
+Clarinet.test({
+    name: "Ensure that burn function is working",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get('deployer')!;
+        const userA = accounts.get('wallet_1')!;
+        const Price = 50;
+        const { paymentAssetContract, paymentAssetId } = mintFt({ chain, deployer, amount: 100, token_id: 101, supply: 20, recipient: deployer});
+        
+        let call = chain.mineBlock([
+            Tx.contractCall(
+                'token-option',
+                'burn',
+                [ 
+                    types.uint(101),
+                    types.uint(10),
+                    types.principal(deployer.address),
+                    types.uint(20),
+                    
+                ],
+                deployer.address)
+        ]);
+
+        call.receipts[0].result.expectOk();
+        call.receipts[0].events.expectFungibleTokenBurnEvent(10, deployer.address, paymentAssetId);
+
+
+    },
+});
+
+Clarinet.test({
+    name: "Ensure that minting function is only called by the contract owner",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get('deployer')!;
+        const userA = accounts.get('wallet_1')!;
+        
+        let call = chain.mineBlock([
+            Tx.contractCall(
+                'token-option',
+                'mint',
+                [ 
+                    types.uint(1000),
+                    types.uint(10),
+                    types.principal(userA.address),
+                    types.uint(20),
+                ],
+                userA.address),
+        ]);
+
+        call.receipts[0].result.expectErr().expectUint(ERR_CONTRACT_OWNER_ONLY);
+    },
+});
+
+Clarinet.test({
+    name: "Ensure that amount is greater than 0 while minting",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get('deployer')!;
+        const userA = accounts.get('wallet_1')!;
+        
+        let call = chain.mineBlock([
+            Tx.contractCall(
+                'token-option',
+                'mint',
+                [ 
+                    types.uint(1000),
+                    types.uint(0),
+                    types.principal(userA.address),
+                    types.uint(20),
+                ],
+                deployer.address),
+        ]);
+
+        call.receipts[0].result.expectErr().expectUint(ERR_INSUFFICIENT_TOKENS_TO_MINT);
+    },
+});
+
+Clarinet.test({
+    name: "Ensure that burn doesn't work on less balance",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get('deployer')!;
+        const userA = accounts.get('wallet_1')!;
+        
+        let readOnlyCall = chain.callReadOnlyFn('token-option', 'get-balance', [types.uint(10), types.principal(deployer.address)], deployer.address);
+        readOnlyCall.result.expectOk().expectUint(0);
+        
+
+        let call = chain.mineBlock([
+            Tx.contractCall(
+                'token-option',
+                'burn',
+                [ 
+                    types.uint(1000),
+                    types.uint(10),
+                    types.principal(deployer.address),
+                    types.uint(20),
+                ],
+                deployer.address),
+        ]);
+
+        call.receipts[0].result.expectErr().expectUint(ERR_INSUFFICIENT_BALANCE);
     },
 });
