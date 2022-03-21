fb8fc77b1d46376ba8e430a033e38eb255d63363
luizalbertocviana <luizalbertocviana@gmail.com>
2022-03-21 16:35:30 -0300

    introduces error constants in vault_test.ts

--- tests/vault_test.ts
+++ tests/vault_test.ts
@@ -1,6 +1,13 @@
 
 import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v0.14.0/index.ts';
 
+const ERR_NOT_AUTHORIZED = 1000;
+const ERR_NOT_NEW_OWNER = 2000;
+const ERR_OWNERSHIP_TRANSFER_ALREADY_SUBMITTED = 2001;
+const ERR_NO_OWNERSHIP_TRANSFER_TO_CANCEL = 2002;
+const ERR_NO_OWNERSHIP_TRANSFER_TO_CONFIRM = 2003;
+const ERR_TOO_SOON_TO_WITHDRAW = 4000;
+
 Clarinet.test({
     name: "Ensure that submit ownership can only be called by owner",
     fn(chain: Chain, accounts: Map<string, Account>) {
@@ -15,7 +22,7 @@ Clarinet.test({
                 [types.principal(userB.address)],
                 userB.address)
             ]);
-        call.receipts[0].result.expectErr().expectUint(1000);
+        call.receipts[0].result.expectErr().expectUint(ERR_NOT_AUTHORIZED);
     }
 })
 
@@ -37,7 +44,7 @@ Clarinet.test({
                 [types.principal(userB.address)],
                 deployer.address)
             ]);
-        call.receipts[1].result.expectErr().expectUint(2001);
+        call.receipts[1].result.expectErr().expectUint(ERR_OWNERSHIP_TRANSFER_ALREADY_SUBMITTED);
     }
 })
 
@@ -55,7 +62,7 @@ Clarinet.test({
                 [],
                 userB.address)
             ]);
-        call.receipts[0].result.expectErr().expectUint(1000);
+        call.receipts[0].result.expectErr().expectUint(ERR_NOT_AUTHORIZED);
     }
 })
 
@@ -73,7 +80,7 @@ Clarinet.test({
                 [],
                 deployer.address)
             ]);
-        call.receipts[0].result.expectErr().expectUint(2002);
+        call.receipts[0].result.expectErr().expectUint(ERR_NO_OWNERSHIP_TRANSFER_TO_CANCEL);
     }
 })
 
@@ -91,7 +98,7 @@ Clarinet.test({
                 [],
                 userA.address)
             ]);
-        call.receipts[0].result.expectErr().expectUint(2003);
+        call.receipts[0].result.expectErr().expectUint(ERR_NO_OWNERSHIP_TRANSFER_TO_CONFIRM);
     }
 })
 
@@ -113,7 +120,7 @@ Clarinet.test({
                 [],
                 userA.address)
             ]);
-        call.receipts[1].result.expectErr().expectUint(2000);
+        call.receipts[1].result.expectErr().expectUint(ERR_NOT_NEW_OWNER);
     }
 })
 
