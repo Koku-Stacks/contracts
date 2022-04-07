c727e0b2a991e8d9dcc4087e0eef94367a875be8
luizalbertocviana <luizalbertocviana@gmail.com>
2022-04-07 14:08:32 -0300

    Revert "moves and adapts some unit tests from vault_test.ts to timestamp-vault-accessor_test.ts"
    This reverts commit ff344849900a9ff341d54dedc358c7176a18865f.

--- tests/vault_test.ts
+++ tests/vault_test.ts
@@ -5,6 +5,7 @@ const ERR_NOT_NEW_OWNER = 2000;
 const ERR_OWNERSHIP_TRANSFER_ALREADY_SUBMITTED = 2001;
 const ERR_NO_OWNERSHIP_TRANSFER_TO_CANCEL = 2002;
 const ERR_NO_OWNERSHIP_TRANSFER_TO_CONFIRM = 2003;
+const ERR_TOO_SOON_TO_WITHDRAW = 4000;
 
 Clarinet.test({
     name: "Ensure that submit ownership can only be called by owner",
@@ -120,3 +121,147 @@ Clarinet.test({
         call.receipts[1].result.expectErr().expectUint(ERR_NOT_NEW_OWNER);
     }
 })
+
+Clarinet.test({
+    name: "Ensure that deposit and withdraw are working fine",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get('deployer')!;
+        const userA = accounts.get('wallet_1')!;
+        const vault = `${deployer.address}.vault`;
+
+        let call1 = chain.mineBlock([
+            Tx.contractCall(
+                'token',
+                'add-authorized-contract',
+                [
+                    types.principal(deployer.address),
+                ],
+                deployer.address),
+            Tx.contractCall(
+                'token',
+                'mint',
+                [
+                    types.uint(1000),
+                    types.principal(userA.address),
+                ],
+                deployer.address),
+            Tx.contractCall(
+                'vault',
+                'deposit',
+                [
+                    types.uint(100),
+                    types.none()
+                ],
+                userA.address)
+        ]);
+
+        call1.receipts[0].result.expectOk().expectBool(true)
+        call1.receipts[1].result.expectOk().expectBool(true)
+        call1.receipts[2].result.expectOk().expectBool(true)
+
+        let balance = chain.callReadOnlyFn('token', 'get-balance', [types.principal(userA.address)], userA.address);
+        balance.result.expectOk().expectUint(900);
+
+        balance = chain.callReadOnlyFn('token', 'get-balance', [types.principal(vault)], userA.address);
+        balance.result.expectOk().expectUint(100);
+
+        let call2 = chain.mineBlock([
+            Tx.contractCall(
+                'vault',
+                'withdraw',
+                [
+                    types.uint(100),
+                    types.none()
+                ],
+                userA.address)
+        ]);
+
+        // call2.receipts[0].result.expectOk();
+
+        // balance = chain.callReadOnlyFn('token', 'get-balance', [types.principal(userA.address)], userA.address);
+        // balance.result.expectOk().expectUint(1000);
+
+        // balance = chain.callReadOnlyFn('token', 'get-balance', [types.principal(vault)], userA.address);
+        // balance.result.expectOk().expectUint(0);
+    },
+});
+
+Clarinet.test({
+    name: "Ensure the cooldown mechanism works as expected",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        const deployer = accounts.get('deployer')!;
+        const userA = accounts.get('wallet_1')!;
+
+        const call1 = chain.mineBlock([
+            Tx.contractCall(
+                'token',
+                'add-authorized-contract',
+                [
+                    types.principal(deployer.address),
+                ],
+                deployer.address),
+            Tx.contractCall(
+                'token',
+                'mint',
+                [
+                    types.uint(1000),
+                    types.principal(userA.address),
+                ],
+                deployer.address),
+            Tx.contractCall(
+                'vault',
+                'set-cooldown',
+                [
+                    types.uint(1),
+                ],
+                deployer.address)
+        ]);
+
+        call1.receipts[0].result.expectOk().expectBool(true);
+        call1.receipts[1].result.expectOk().expectBool(true);
+        call1.receipts[2].result.expectOk().expectBool(true);
+
+        const call2 = chain.mineBlock([
+            Tx.contractCall(
+                'vault',
+                'deposit',
+                [
+                    types.uint(100),
+                    types.none()
+                ],
+                userA.address)
+        ]);
+
+        call2.receipts[0].result.expectOk().expectBool(true);
+
+        const call3 = chain.mineBlock([
+            Tx.contractCall(
+                'vault',
+                'withdraw',
+                [
+                    types.uint(10),
+                    types.none()
+                ],
+                userA.address
+            )
+        ]);
+
+        call3.receipts[0].result.expectErr().expectUint(ERR_TOO_SOON_TO_WITHDRAW);
+
+        chain.mineEmptyBlock(100);
+
+        const call4 = chain.mineBlock([
+            Tx.contractCall(
+                'vault',
+                'withdraw',
+                [
+                    types.uint(10),
+                    types.none()
+                ],
+                userA.address
+            )
+        ]);
+
+        // call4.receipts[0].result.expectOk();
+    },
+});
