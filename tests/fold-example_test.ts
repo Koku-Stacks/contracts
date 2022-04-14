23471132a6ea5e76567ec00186f081c9f6c8eebc
luizalbertocviana <luizalbertocviana@gmail.com>
2022-04-14 16:11:35 -0300

    adds a unit test regarding batch update facilities

--- tests/fold-example_test.ts
+++ tests/fold-example_test.ts
@@ -75,3 +75,140 @@ Clarinet.test({
         data_indices.map(idx => data_verification_calls[idx - 1].result.expectUint(data[idx - 1] * 10 + amount_to_increase));
     }
 });
+
+Clarinet.test({
+    name: "Ensures batch update operation works as expected",
+    fn(chain: Chain, accounts: Map<string, Account>) {
+        const userA = accounts.get('wallet_1')!;
+
+        const chunk_size = 100
+
+        const data_size = 2 * chunk_size; // twice the chunk size so we call update twice
+
+        const data_indices = Array.from(Array(data_size).keys()).map(idx => idx + 1);
+
+        const constant_data_to_insert = 100;
+
+        const data_inserter_call = (data: number) => Tx.contractCall(
+            'fold-example',
+            'insert-data',
+            [types.uint(data), types.uint(data * 10)],
+            userA.address
+        );
+
+        let call = chain.mineBlock(
+            data_indices.map(
+                _idx => data_inserter_call(constant_data_to_insert)
+            )
+        );
+
+        call.receipts.map(receipt => receipt.result.expectOk().expectBool(true));
+
+        let data_verification_calls = data_indices.map(idx => chain.callReadOnlyFn(
+            'fold-example',
+            'get-a',
+            [types.uint(idx), types.uint(0)],
+            userA.address
+        ));
+        data_verification_calls.map(verification_call => verification_call.result.expectUint(constant_data_to_insert));
+
+        data_verification_calls = data_indices.map(idx => chain.callReadOnlyFn(
+            'fold-example',
+            'get-b',
+            [types.uint(idx), types.uint(0)],
+            userA.address
+        ));
+        data_verification_calls.map(verification_call => verification_call.result.expectUint(constant_data_to_insert * 10));
+
+        const amount_to_add_on_update = 3;
+
+        call = chain.mineBlock([
+            Tx.contractCall(
+                'fold-example',
+                'update-next-index-chunk',
+                [types.uint(amount_to_add_on_update)],
+                userA.address
+            )
+        ]);
+
+        call.receipts[0].result.expectOk().expectBool(true);
+
+        const indices_from_first_chunk = data_indices.slice(0, chunk_size);
+        const indices_from_second_chunk = data_indices.slice(chunk_size);
+
+        data_verification_calls = indices_from_first_chunk.map(idx => chain.callReadOnlyFn(
+            'fold-example',
+            'get-a',
+            [types.uint(idx), types.uint(0)],
+            userA.address
+        ));
+        data_verification_calls.map(verification_call => verification_call.result.expectUint(constant_data_to_insert + amount_to_add_on_update));
+
+        data_verification_calls = indices_from_second_chunk.map(idx => chain.callReadOnlyFn(
+            'fold-example',
+            'get-a',
+            [types.uint(idx), types.uint(0)],
+            userA.address
+        ));
+        data_verification_calls.map(verification_call => verification_call.result.expectUint(constant_data_to_insert));
+
+        data_verification_calls = indices_from_first_chunk.map(idx => chain.callReadOnlyFn(
+            'fold-example',
+            'get-b',
+            [types.uint(idx), types.uint(0)],
+            userA.address
+        ));
+        data_verification_calls.map(verification_call => verification_call.result.expectUint(constant_data_to_insert * 10 + amount_to_add_on_update));
+
+        data_verification_calls = indices_from_second_chunk.map(idx => chain.callReadOnlyFn(
+            'fold-example',
+            'get-b',
+            [types.uint(idx), types.uint(0)],
+            userA.address
+        ));
+        data_verification_calls.map(verification_call => verification_call.result.expectUint(constant_data_to_insert * 10));
+
+        call = chain.mineBlock([
+            Tx.contractCall(
+                'fold-example',
+                'update-next-index-chunk',
+                [types.uint(amount_to_add_on_update)],
+                userA.address
+            )
+        ]);
+
+        call.receipts[0].result.expectOk().expectBool(true);
+
+        data_verification_calls = indices_from_first_chunk.map(idx => chain.callReadOnlyFn(
+            'fold-example',
+            'get-a',
+            [types.uint(idx), types.uint(0)],
+            userA.address
+        ));
+        data_verification_calls.map(verification_call => verification_call.result.expectUint(constant_data_to_insert + amount_to_add_on_update));
+
+        data_verification_calls = indices_from_second_chunk.map(idx => chain.callReadOnlyFn(
+            'fold-example',
+            'get-a',
+            [types.uint(idx), types.uint(0)],
+            userA.address
+        ));
+        data_verification_calls.map(verification_call => verification_call.result.expectUint(constant_data_to_insert + amount_to_add_on_update));
+
+        data_verification_calls = indices_from_first_chunk.map(idx => chain.callReadOnlyFn(
+            'fold-example',
+            'get-b',
+            [types.uint(idx), types.uint(0)],
+            userA.address
+        ));
+        data_verification_calls.map(verification_call => verification_call.result.expectUint(constant_data_to_insert * 10 + amount_to_add_on_update));
+
+        data_verification_calls = indices_from_second_chunk.map(idx => chain.callReadOnlyFn(
+            'fold-example',
+            'get-b',
+            [types.uint(idx), types.uint(0)],
+            userA.address
+        ));
+        data_verification_calls.map(verification_call => verification_call.result.expectUint(constant_data_to_insert * 10 + amount_to_add_on_update));
+    }
+})
