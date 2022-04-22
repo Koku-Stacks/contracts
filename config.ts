a9fe43a8df0613e0ae3f7826eb3987c4aab0ffb0
luizalbertocviana <luizalbertocviana@gmail.com>
2022-04-22 15:48:30 -0300

    adds config parameter about where to store deployed contract addresses

--- config.ts
+++ config.ts
@@ -5,6 +5,8 @@ export const network_type = "testnet";
 export const node_url = "http://3.64.221.107:3999";
 export const default_fee = 100000;
 
+export const contract_id_info_filename = "contracts/addresses.json"
+
 export function get_transaction_version(network_type: string): TransactionVersion {
     if (network_type === 'mainnet') {
         return TransactionVersion.Mainnet;
