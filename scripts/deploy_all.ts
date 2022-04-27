be2c801855a6d01b570d62be0cd06154977ea73b
kevinamexo <bangitskev@gmail.com>
2022-04-27 11:29:57 +0100

    added function to mint to chain accounts

--- scripts/deploy_all.ts
+++ scripts/deploy_all.ts
@@ -1,72 +1,96 @@
-import { readFileSync, writeFileSync } from "fs"
+import { readFileSync, writeFileSync } from "fs";
 
-import { principalCV } from "@stacks/transactions/dist/clarity/types/principalCV";
+import { principalCV } from " @stacks/transactions/dist/clarity/types/principalCV";
 import { chain, deploy_contract } from "./deploy_utils";
 
 import * as config from "../config";
+import { uintCV } from "@stacks/transactions";
+import { Account } from "../integration/framework/stacks.chain";
 
 function read_deployment_order(filename?: string): string[] {
-    filename = filename ?? config.deployment_order_filename;
+  filename = filename ?? config.deployment_order_filename;
 
-    const content = readFileSync(filename);
-    
-    const deployment_order = content.toString().split('\n');
+  const content = readFileSync(filename);
 
-    return deployment_order;
+  const deployment_order = content.toString().split("\n");
+
+  return deployment_order;
 }
 
 async function deploy_all_contracts() {
-    let contracts_id_registry: Map<string, string> = new Map();
+  let contracts_id_registry: Map<string, string> = new Map();
 
-    for (const contract_name of read_deployment_order()) {
-        const contract_id = await deploy_contract(contract_name);
+  for (const contract_name of read_deployment_order()) {
+    const contract_id = await deploy_contract(contract_name);
 
-        console.log(`${contract_id} successfully deployed`);
+    console.log(`${contract_id} successfully deployed`);
 
-        contracts_id_registry[contract_name] = contract_id;
-    }
+    contracts_id_registry[contract_name] = contract_id;
+  }
 
-    return contracts_id_registry;
+  return contracts_id_registry;
 }
 
 async function post_deployment_transactions() {
-    await chain.loadAccounts();
+  await chain.loadAccounts();
 
-    const deployer = chain.accounts.get("deployer")!;
+  const deployer = chain.accounts.get("deployer")!;
 
+  await chain.callContract(
+    deployer.address,
+    "token",
+    "add-authorized-contract",
+    [principalCV(`${deployer.address}.minting`)],
+    deployer.secretKey,
+    {
+      waitForTransactionConfirmation: true,
+    }
+  );
+
+  await chain.callContract(
+    deployer.address,
+    "token",
+    "add-authorized-contract",
+    [principalCV(deployer.address)],
+    deployer.secretKey,
+    {
+      waitForTransactionConfirmation: true,
+    }
+  );
+
+  async function mint(wallet: string) {
     await chain.callContract(
-        deployer.address,
-        "token",
-        "add-authorized-contract",
-        [principalCV(`${deployer.address}.minting`)],
-        deployer.secretKey,
-        {
-            waitForTransactionConfirmation: true
-        }
+      deployer.address,
+      "token",
+      "mint",
+      [uintCV(1000000), principalCV(wallet)],
+      deployer.secretKey,
+      {
+        waitForTransactionConfirmation: true,
+      }
     );
+  }
+  const accountNames = Array.from(chain.accounts.keys());
+  const accountDetails: Array<Account> = accountNames.map((name) => {
+    return chain.accounts.get(name);
+  });
 
-    await chain.callContract(
-        deployer.address,
-        "token",
-        "add-authorized-contract",
-        [principalCV(deployer.address)],
-        deployer.secretKey,
-        {
-            waitForTransactionConfirmation: true
-        }
-    );
+  await Promise.all([accountDetails.map((account) => mint(account.address))]);
 }
 
-function register_contracts_id(contracts_id_registry: Map<string, string>, filename?: string) {
-    filename = filename ?? config.contract_id_info_filename;
+function register_contracts_id(
+  contracts_id_registry: Map<string, string>,
+  filename?: string
+) {
+  filename = filename ?? config.contract_id_info_filename;
 
-    writeFileSync(filename, JSON.stringify(contracts_id_registry, null, '\t'));
+  writeFileSync(filename, JSON.stringify(contracts_id_registry, null, "\t"));
 }
 
 async function service() {
-    const contracts_id_registry =  await deploy_all_contracts();
-    await post_deployment_transactions();
-    register_contracts_id(contracts_id_registry);
+  const contracts_id_registry = await deploy_all_contracts();
+  await post_deployment_transactions();
+  register_contracts_id(contracts_id_registry);
 }
 
 service();
