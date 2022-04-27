431d3989f142fa393f81750bff43d06a61625f1c
luizalbertocviana <luizalbertocviana@gmail.com>
2022-04-27 16:30:21 -0300

    adds an export constant in config in order to avoid a hardcoded string in deploy_all.ts

--- config.ts
+++ config.ts
@@ -4,6 +4,7 @@ export const node_url = "http://3.64.221.107:3999";
 export const default_fee = 100000;
 
 export const contract_id_info_filename = "contracts/addresses.json"
+export const deployment_order_filename = "contracts/deployment_order.txt";
 
 export function get_transaction_version(network_type: string): TransactionVersion {
     if (network_type === 'mainnet') {
