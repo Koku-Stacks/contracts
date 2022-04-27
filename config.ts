78461bd4da8cdc920f502a5360d19a60301d2a35
luizalbertocviana <luizalbertocviana@gmail.com>
2022-04-27 16:29:38 -0300

    removes two unused constants. Every script has been chacked and run after such a removal

--- config.ts
+++ config.ts
@@ -1,7 +1,5 @@
 import { TransactionVersion } from "@stacks/transactions";
 
-export const seed_phrase = "twice kind fence tip hidden tilt action fragile skin nothing glory cousin green tomorrow spring wrist shed math olympic multiply hip blue scout claw";
-export const network_type = "testnet";
 export const node_url = "http://3.64.221.107:3999";
 export const default_fee = 100000;
 
