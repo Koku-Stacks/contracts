5a5e942f35984731a0ffdadf5e495e1e4ae74461
luizalbertocviana <luizalbertocviana@gmail.com>
2022-05-09 14:24:10 -0300

    Revert "temporarily removes events integration test"
    This reverts commit bf6f4e39e2329fa9b9a0bb05a1f05148ea96d67a.

---
+++ integration_tests/test_pool/events.test.ts
@@ -0,0 +1,26 @@
+import { STACKS_API_URL } from "../config";
+import { StacksChain } from "dy-finance.lib";
+const chain = new StacksChain(STACKS_API_URL, {
+  defaultFee: 100000,
+});
+const enum Event {
+  smart_contract_log = "smart_contract_log",
+  non_fungible_token_asset = "non_fungible_token_asset",
+  fungible_token_asset = "fungible_token_asset",
+  stx_lock = "stx_lock",
+  stx_asset = "stx_asset",
+}
+describe("events contract", () => {
+  before(async () => {
+    await chain.loadAccounts();
+  });
+
+  it("Ensures print is working", async () => {
+    const deployer = chain.accounts.get("deployer")!;
+    const txns_ids = await chain.getEventsByContract(
+      deployer.address,
+      Event.fungible_token_asset
+    );
+    console.log(txns_ids);
+  });
+});
